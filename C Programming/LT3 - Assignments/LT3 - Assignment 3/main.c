e2d2ae3d2c47bcaa956015e16a4f05cacacdeb4a
Abdallah Khater <abdallahahmed363@gmail.com>
2020-08-30 14:26:38 +0200

    C Programming assignment#3

---
+++ C Programming/LT3 - Assignments/LT3 - Assignment 3/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 29, 2020
+ *      Author: Abdallah Khater
+ */
+
+
+#include "stdio.h"
+
+int main()
+{
+	int Num1 , Num2;
+	printf("Enter two integers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d", &Num1, &Num2);
+	printf("Sum: %d", Num1 + Num2);
+	return 0;
+
+}
