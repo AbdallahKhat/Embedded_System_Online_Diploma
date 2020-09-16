1dfc7536a46891910bb8e2d6e4e569063ce5dda6
Abdallah Khater <abdallahahmed363@gmail.com>
2020-09-17 01:17:07 +0200

    LT5 - Assign 3

---
+++ C Programming/LT5 - Assignments/Reverse_Sentence.c
@@ -0,0 +1,38 @@
+/*
+ * Reverse_Sentence.c
+ *
+ *  Created on: Sep 17, 2020
+ *      Author: Abdallah Khater
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+//C Program to Reverse a Sentence Using Recursion
+
+void reverse();
+
+int main()
+{
+	char str[100];
+	int length;
+
+	printf("Enter a sentence: ");
+	gets(str);
+
+	length = strlen(str);
+
+	reverse(str , length);
+
+	return 0;
+}
+
+void reverse(char str[] , int i)
+{
+	if(i != 0)
+	{
+		printf("%c", str[i-1]);
+		reverse(str , i-1);
+	}
+}
