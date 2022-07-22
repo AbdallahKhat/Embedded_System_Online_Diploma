2ec35aa57113feb55f72cd8449346a71426d8e94
Abdallah Khater <abdallahahmed363@gmail.com>
2022-07-22 21:37:34 +0200

    Another solution for swapping 2 num w/o temp using XOR

--- C Programming/LT3 - Assignments/LT3 - Assignment 7/main.c
+++ C Programming/LT3 - Assignments/LT3 - Assignment 7/main.c
@@ -28,3 +28,19 @@ int main()
 	printf("\nAfter swapping, value of b = %f", b);
 	return 0;
 }
+
+/*
+//Another method for swapping 2 numbers w/o temp, using XOR
+int main()
+{
+	int n1=1,n2=2;
+
+	n1^=n2; //bitwise 0001 xor 0010 = 0011 (3)
+	n2^=n1; //        0010 xor 0011 = 0001 (1)
+	n1^=n2; //        0011 xor 0001 = 0010 (2)
+
+	printf("%d %d",n1,n2);
+
+	return 0;
+}
+*/
