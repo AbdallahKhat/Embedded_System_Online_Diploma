c4d57ffb3ed37dbe179dc291f0b2e333f869ab60
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-03-31 04:53:24 +0200

    Refactor Unit03 Embedded C folder structure for clarity
    - Renamed directories to reflect purpose and progression:
      - `01_Bare_Metal_Basics` for foundational concepts.
      - `02_Startup_Linker_UART_Basics` for startup and linker introduction.
      - `03_Assembly_Startup_Linker_Makefile` for assembly-based startup.
      - `04_C_Startup_First_Method` for Advanced C-based startup.
      - `05_C_Startup_Second_Method` for Another C-based startup.
    - Improved organization to align with the learning path.

--- Unit03 Embedded C/01_Bare_Metal_Basics/Led_Toggle_STM32F103C6.c
+++ Unit03 Embedded C/01_Bare_Metal_Basics/Led_Toggle_STM32F103C6.c
@@ -19,36 +19,35 @@
 #include <stdint.h>
 
 
-#define RCC_Base   0x40021000
-#define PortA_Base 0x40010800
-#define RCC_APB2ENR    (RCC_Base + 0x18)
-#define PortA_CRH  (PortA_Base + 0x04)
-#define PortA_ODR  (PortA_Base + 0x0c)
+#define RCC_Base        0x40021000
+#define PortA_Base      0x40010800
+#define RCC_APB2ENR     (RCC_Base + 0x18)
+#define PortA_CRH       (PortA_Base + 0x04)
+#define PortA_ODR       (PortA_Base + 0x0c)
 
 typedef union
 {
-	volatile uint32_t all_ports;
+	volatile uint32_t    all_ports;
 
 	struct
 	{
-		volatile uint32_t pinsA:8;
-		volatile uint32_t pinsB:8;
-		volatile uint32_t pinsC:8;
-		volatile uint32_t pinsD:8;
+		volatile uint32_t    pinsA:8;
+		volatile uint32_t    pinsB:8;
+		volatile uint32_t    pinsC:8;
+		volatile uint32_t    pinsD:8;
 	}pins;
 
 }Reg;
 
 int main(void)
 {
+	volatile Reg* pAPB2ENR    = (volatile Reg*)RCC_APB2ENR;
+	volatile Reg* pPortA_CRH  = (volatile Reg*)PortA_CRH;
+	volatile Reg* pPortA_ODR  = (volatile Reg*)PortA_ODR;
 
-	volatile Reg* pAPB2ENR = (volatile Reg*)RCC_APB2ENR;
-	volatile Reg* pPortA_CRH = (volatile Reg*)PortA_CRH;
-	volatile Reg* pPortA_ODR = (volatile Reg*)PortA_ODR;
-
-	pAPB2ENR->pins.pinsA |= 2;
-	pPortA_CRH->pins.pinsD &= 0x0f;
-	pPortA_CRH->pins.pinsD |= 0x20;
+	pAPB2ENR->pins.pinsA     |= 2;
+	pPortA_CRH->pins.pinsD   &= 0x0f;
+	pPortA_CRH->pins.pinsD   |= 0x20;
 
 	while(1)
 	{
