7d16ce21272d87cad26a202885efaebf0c24d32a
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-03-31 14:28:58 +0200

    Refactor Unit07 MCU Essential Peripherals folder structure for clarity
    - Renamed folders to reflect their contents.
    - Organized subfolders for better structure.
    - cleaned up build artifacts, debug configurations, and unnecessary files.

--- Unit7 MCU Essential Peripherals/LT4/Drivers/Debug/STM32_F103C6_Drivers/GPIO/STM32_F103C6_GPIO_Driver.d
+++
@@ -1,6 +0,0 @@
-STM32_F103C6_Drivers/GPIO/STM32_F103C6_GPIO_Driver.o: \
- ../STM32_F103C6_Drivers/GPIO/STM32_F103C6_GPIO_Driver.c \
- D:/Embedded_Systems/Workspace/STM32CubeIDE/Embedded\ C/Drivers/STM32_F103C6_Drivers/Inc/STM32_F103C6_GPIO_Driver.h \
- D:/Embedded_Systems/Workspace/STM32CubeIDE/Embedded\ C/Drivers/STM32_F103C6_Drivers/Inc/STM32F103x6.h
-D:/Embedded_Systems/Workspace/STM32CubeIDE/Embedded\ C/Drivers/STM32_F103C6_Drivers/Inc/STM32_F103C6_GPIO_Driver.h:
-D:/Embedded_Systems/Workspace/STM32CubeIDE/Embedded\ C/Drivers/STM32_F103C6_Drivers/Inc/STM32F103x6.h:
